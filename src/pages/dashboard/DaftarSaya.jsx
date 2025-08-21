28013a4146b68caedc38ac45185076b3aafaa438
Satria Nurrahmat Baihaqi <rahmatsatria088@gmail.com>
2025-08-21 17:53:38 +0700

    update project

--- src/pages/dashboard/DaftarSaya.jsx
+++ src/pages/dashboard/DaftarSaya.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import MyListCard from '../../component/MyListCard';
-import '../../css/moviecard.css';
-import useDaftarSayaStore from './data/useDaftarSayaStore';
+import React from "react";
+import MyListCard from "../../component/MyListCard";
+import "../../css/moviecard.css";
+import useDaftarSayaStore from "./data/useDaftarSayaStore";
 
 const DaftarSaya = () => {
   const { daftar, removeFromDaftar } = useDaftarSayaStore();
 
   return (
-    <div className="row g-3">
+    <div className="movie-grid">
       {daftar.length === 0 ? (
         <p className="text-muted">Belum ada film di daftar kamu.</p>
       ) : (
         daftar.map((movie) => (
-          <div className="col-6 col-md-4 col-lg-3" key={movie.id}>
-            <MyListCard movie={movie} onRemove={() => removeFromDaftar(movie.id)} />
-          </div>
+          <MyListCard
+            key={movie.id}
+            movie={movie}
+            onRemove={() => removeFromDaftar(movie.id)}
+          />
         ))
       )}
     </div>
