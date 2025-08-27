95b8ea02b7f96ff667ac19d4736146e3962c4c6d
Satria Nurrahmat Baihaqi <rahmatsatria088@gmail.com>
2025-08-27 19:43:49 +0700

    fix: update menu film

--- src/pages/dashboard/Film.jsx
+++ src/pages/dashboard/Film.jsx
@@ -1,5 +1,20 @@
+import kucingProyek from '../../assets/kucing-proyek-foto.png';
+
 function Film() {
-  return <h3>Ini halaman Film.</h3>;
+  return (
+<div style={{ maxWidth: '300px', margin: '0 auto' }}>
+  <img
+    src={kucingProyek}
+    alt="kucing proyek"
+    style={{
+      width: '100%',
+      height: 'auto',
+      objectFit: 'cover',
+      borderRadius: '8px',
+    }}
+  />
+</div>
+  );
 }
 
 export default Film;
